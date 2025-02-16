0d97ba2162e1b449dbe3c55c7e717a05ace2c1cc
ROY . <dkunal665@gmail.com>
2025-02-16 17:31:38 +0530

    updates redirects

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ function Login() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle login logic
+    window.location.pathname = "https://huggingface.co/spaces/Kwai-Kolors/Kolors-Virtual-Try-On"
   };
 
   return (
